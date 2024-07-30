e854d060dac671ab3c1ffeda3b17bd7a4c9fe0d7
Toru Hiyama <toru.hiyama@nttdata.com>
2024-07-30 04:49:15 +0000

    test

---
+++ models/staging/fifa/stg_player.sql
@@ -0,0 +1,30 @@
+with 
+
+source as (
+
+    select * from {{ source('fifa', 'player') }}
+
+),
+
+renamed as (
+
+    select
+        player_id,
+        player_first_name,
+        player_middle_name,
+        player_last_name,
+        concat(player_first_name, ' ', player_last_name) as player_name,
+        datediff(year, birth_date, '2018-06-14') as age,
+        player_known_name,
+        birth_date,
+        weight,
+        height,
+        city,
+        national_team_affiliation_id,
+        affiliation_id
+
+    from source
+
+)
+
+select * from renamed
