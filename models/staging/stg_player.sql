f67106092cd181efbfe45741c0668ea1628517b1
Tim Keegan <tim.keegan@twg.co.nz>
2024-11-07 01:12:35 +0000

    feat: Add player model
    * introduces new model for fifa players

---
+++ models/staging/stg_player.sql
@@ -0,0 +1,19 @@
+with players as (
+    select * from {{ source('fifa', 'player') }}
+)
+
+select
+    id as player_id,
+    player_first_name,
+    player_middle_name,
+    player_last_name,
+    player_known_name,
+    birth_date,
+    weight,
+    height,
+    city,
+    national_team_affiliation_id,
+    affiliation_id,
+    concat(player_first_name, ' ', player_last_name) as player_name,
+    datediff('year', birth_date, '2018-06-14') as age_at_tournament_start
+from players
